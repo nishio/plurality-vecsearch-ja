3102230610693885672638949e752c6ca52abe5e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2024-04-02 21:48:45 +0900

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ const PROJECT_NAME = process.env.PROJECT_NAME!;
 export function Header() {
   return (
     <header className="py-2 px-4 bg-blue-700 text-white shadow-lg">
-      <h1 className="text-xl md:text-2xl font-bold">Omoikane Vector Search</h1>
+      <h1 className="text-xl md:text-2xl font-bold">⿻VecSearchJA</h1>
       <p className="text-xs">
+        Plurality Vector Search in Japanese from{" "}
         <a
           className="hover:underline"
           href="https://scrapbox.io/omoikane/"
